dcc323507fc0839fc9d2d8f2a4e422c82bb3931d
appleEmily <emiri3128@gmail.com>
2019-06-06 01:33:27 +0900

    complete

--- HW3/ViewController.swift
+++ HW3/ViewController.swift
@@ -3,50 +3,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var second:Int!
+    var first:Int!
+    var answer:Int!
     
     @IBOutlet weak var text1: UITextField!
-    
     @IBOutlet weak var text2: UITextField!
-    
     @IBOutlet weak var label: UILabel!
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        text1.keyboardType = UIKeyboardType.numberPad
+        text2.keyboardType = UIKeyboardType.numberPad
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    
-    
-    
     @IBAction func calculate() {
-        //        if text1.text == "", text2.text == "" {
-        //
-        //        } else if text1.text == "" {
-        //        } else if text2.text == "" {
-        //        } else {
-        //            let second:Int = Int(text2.text!)!
-        //            let first:Int = Int(text1.text!)!
-        //
-        //            let answer:Int = first + second
-        //            let result:String = String(Int(answer))
-        //            label.text = result
-        //        }
-        //        print(text1)
-        //        print(text2)
-        //        return
-        //    }
-        //スッキリかけた・文字入れると落ちる
-        if text1.text != "" && text2.text != "" {
-            let second:Int = Int(text2.text!)!
-            let first:Int = Int(text1.text!)!
-            
-            let answer:Int = first + second
-            let result:String = String(Int(answer))
-            label.text = result
-            
+        if  text1.text != "" && text2.text != "" {
+            let result = culculateMesod()
+            label.text = String(result)
         }
-        return
+    }
+    
+    func culculateMesod() -> Int{
+        second = Int(text2.text!)
+        first = Int(text1.text!)
+        answer = first + second
+        return Int(answer)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
